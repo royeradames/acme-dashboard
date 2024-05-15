1a52fa18e390804a07f50ccdbce1b8dadc90c1e1
royeradames <raadamesbonilla@pplweb.com>
2024-05-15 18:47:58 -0400

    update: search and pagination functionality

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,24 +1,23 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
-import {usePathname, useRouter, useSearchParams} from "next/navigation";
-import {useDebouncedCallback} from "use-debounce";
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
-    const searchParams = useSearchParams()
-    const pathName = usePathname()
-    const {replace} = useRouter()
-    const handleSearch = useDebouncedCallback((term: string) => {
-        console.log('Searching...', term)
-        const param = new URLSearchParams(searchParams)
-        if(term){
-            param.set('query', term)
-        }
-        else{
-            param.delete('query')
-        }
-        replace(`${pathName}?${param.toString()}`)
-    }, 300)
+  const searchParams = useSearchParams();
+  const pathName = usePathname();
+  const { replace } = useRouter();
+  const handleSearch = useDebouncedCallback((term: string) => {
+    const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
+    if (term) {
+      params.set('query', term);
+    } else {
+      params.delete('query');
+    }
+    replace(`${pathName}?${params.toString()}`);
+  }, 300);
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
