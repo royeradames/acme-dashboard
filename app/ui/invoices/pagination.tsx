792a55f8c4e41aa795cb18d69296bb4ebfdd43d3
royeradames <raadamesbonilla@pplweb.com>
2024-05-16 08:40:12 -0400

    fix: pagination not working

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -15,7 +15,7 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
   function createPageURL(pageNumber: number | string) {
     const params = new URLSearchParams(searchParams);
     params.set('page', pageNumber.toString());
-    return `${pathname}${params.toString()}`;
+    return `${pathname}?${params.toString()}`;
   }
 
   return (
